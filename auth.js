a028cd5275626235ab738f1cfc59e02638f0c3de
Tanya <tanya.jhaveri@growexx.com>
2026-02-05 16:22:10 +0530

    feat: implement user login with validation

--- auth.js
+++ auth.js
@@ -1,12 +1,3 @@
 function login(username, password) {
-// Validate credentials
-
-}
-
-function logout() {
-console.log("Logging out");
-}
-
-function validatePassword(password) {
-return password.length >= 8;
+// TODO
 }
