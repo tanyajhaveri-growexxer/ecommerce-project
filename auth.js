b910758cd14c63abfe50db7b1ede240bc2d22415
Tanya <tanya.jhaveri@growexx.com>
2026-02-05 15:56:35 +0530

    feat: add password validation helper
    typo fix

--- auth.js
+++ auth.js
@@ -1,7 +1,12 @@
 function login(username, password) {
-// TODO
+// Validate credentials
+
 }
 
 function logout() {
 console.log("Logging out");
+}
+
+function validatePassword(password) {
+return password.length >= 8;
 }
