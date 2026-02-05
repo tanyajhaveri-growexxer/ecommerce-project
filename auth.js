edfacb47c5cca5c0d55f4127cd8d52e98e7f066e
Tanya <tanya.jhaveri@growexx.com>
2026-02-05 16:23:08 +0530

    feat: add password validation helper

--- auth.js
+++ auth.js
@@ -1,7 +1,11 @@
 function login(username, password) {
-// TODO
+// Validate credentials
 }
 
 function logout() {
 console.log("Logging out");
+}
+
+function validatePassword(password) {
+return password.length >= 8;
 }
