1ed6f4191b3b5bf121ea6c38807cb0f891763e36
Tanya <tanya.jhaveri@growexx.com>
2026-02-05 16:37:11 +0530

    Add products API endpoint

--- app.js
+++ app.js
@@ -7,4 +7,10 @@ module.exports = app;
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
 console.log(`Server running on port ${PORT}`);
+app.get('/api/users', (req, res) => {
+res.json({ users: [] });
 });
+app.get('/api/products', (req, res) => {
+res.json({ products: [] });
+});
+})
